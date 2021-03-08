0122034ca8e037568f18713ec2a9d8530029d0d6
Mathias Artur Schulz <MathiasSchulz34@gmail.com>
2021-03-08 09:17:20 -0300

    [ADD] Adicionado as tres paginas, Verificacao, Informacao e Sobre e adicionado dois assets sobre logo e governo

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,255 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
+// style dos titulos
+const optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+// número de notícias verificadas
+var numNoticias = 19897;
+
+// widget principal da aplicação
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Start(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+class Start extends StatefulWidget {
+  Start({Key key}) : super(key: key);
+  _Start createState() => _Start();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _Start extends State {
+  // variavel que controla a página atual
+  var _currentPage = 0;
+  var _pages = [
+    PageVerificacao(),
+    PageInformacoes(),
+    PageSobre(),
+  ];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Detecção de Fake News')),
+        body: Center(child: _pages.elementAt(_currentPage)),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.check_box_outlined),
+              label: 'Verificação',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            BottomNavigationBarItem(
+              icon: Icon(Icons.assessment_outlined),
+              label: 'Informações',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.assignment_ind_rounded),
+              label: 'Sobre',
             ),
           ],
+          currentIndex: _currentPage,
+          fixedColor: Colors.teal,
+          onTap: (int inIndex) {
+            setState(() {
+              _currentPage = inIndex;
+            });
+          }
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+    );
+  }
+}
+
+// objeto Noticia
+class Noticia {
+  String titulo = '';
+  String texto = '';
+  String email = '';
+}
+
+// pagina de verificação
+// ignore: must_be_immutable
+class PageVerificacao extends StatelessWidget {
+  GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+  Noticia _noticia = new Noticia();
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(right: 20, left: 20, top: 50),
+        child: Form(
+          key: this._formKey,
+          child: Column(
+            children: [
+              // verificacao
+              Text('Verificação', style: optionStyle),
+              // campo nome
+              TextFormField(
+                keyboardType: TextInputType.multiline,
+                minLines: 6,
+                maxLines: null,
+                validator: (String value) {
+                  if (value.length == 0) {
+                    return 'O campo de nome é obrigatório';
+                  }
+                  return null;
+                },
+                onSaved: (String value) {
+                  this._noticia.texto = value;
+                },
+                decoration: InputDecoration(
+                    hintText: '',
+                    labelText: 'Texto',
+                ),
+              ),
+              // // campo cpf
+              // TextFormField(
+              //   keyboardType: TextInputType.text,
+              //   validator: (String value) {
+              //     print(value);
+              //     if (value.length != 11) {
+              //       return 'O campo de CPF é obrigatório e deve possuir 11 caracteres';
+              //     }
+              //     return null;
+              //   },
+              //   onSaved: (String value) {
+              //     this._usuario.cpf = value;
+              //   },
+              //   decoration: InputDecoration(
+              //       hintText: 'xxxyyyzzzww',
+              //       labelText: 'CPF (Sem pontuação)',
+              //   ),
+              // ),
+              // // campo email
+              // TextFormField(
+              //   keyboardType: TextInputType.name,
+              //   validator: (String value) {
+              //     print(value);
+              //     if (value.length == 0) {
+              //       return 'O campo de e-mail é obrigatório';
+              //     }
+              //     return null;
+              //   },
+              //   onSaved: (String value) {
+              //     this._usuario.email = value;
+              //   },
+              //   decoration: InputDecoration(
+              //       hintText: 'nome@servidor',
+              //       labelText: 'E-mail',
+              //   ),
+              // ),
+              // // botão
+              // Padding(
+              //   padding: EdgeInsets.only(top: 16, left: 250),
+              //   child: ElevatedButton(
+              //     onPressed: () {
+              //       if (_formKey.currentState.validate()) {
+              //         // informa uma mensagem de sucesso
+              //         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              //           content: Text('Vacinação registrada com sucesso! '),
+              //           action: SnackBarAction(label: 'Fechar', onPressed:(){}),
+              //         ));
+              //         // apresenta algumas informações
+              //         print('=> Cadastro do usuário: ');
+              //         print('Nome: ${_usuario.nome}');
+              //         print('E-mail: ${_usuario.email}');
+              //         print('CPF: ${_usuario.cpf}');
+              //         _formKey.currentState.save();
+              //         numVacinas += 1;
+              //         // reseta o formulário
+              //         _formKey.currentState.reset();
+              //       }
+              //     },
+              //     child: Text('Gravar'),
+              //   ),
+              // ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// pagina de informações sobre as notícias
+// ignore: must_be_immutable
+class PageInformacoes extends StatelessWidget {
+  GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(right: 20, left: 20, top: 50),
+        child: Form(
+          key: this._formKey,
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.center,
+                child: Text('Informações', style: optionStyle),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 30),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Número de notícias verificadas: '
+                       + numNoticias.toString(),
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// pagina sobre as informações do aplicativo
+// ignore: must_be_immutable
+class PageSobre extends StatelessWidget {
+  GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(right: 20, left: 20, top: 50),
+        child: Form(
+          key: this._formKey,
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.center,
+                child: Text('Sobre', style: optionStyle),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 30, bottom: 30),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Mathias Artur Schulz',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Image.asset(
+                'imagens/logo.png',
+              ),
+              Image.asset(
+                'imagens/governo.jpg',
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
